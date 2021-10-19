99320b61563df0c2f6eb396af001edca60e5550e
dragoon8192_mac <dragoon8192@gmail.com>
2021-10-20 00:16:04 +0900

    update:   src/PendPhysModel.hs

--- src/PendPhysModel.hs
+++ src/PendPhysModel.hs
@@ -1,43 +1,43 @@
 module PendPhysModel () where
 import Data.AffineSpace
 import Data.VectorSpace
+import Control.Arrow
 import Data.Functor
+import Control.Monad
+import Control.Monad.Reader
 
 type DTime = Double
-data Pendulum q p = Pendulum {
+data Pendulum a = Pendulum {
                 mass    :: Double,
                 length  :: Double,
-                theta   :: q,
-                pTheta  :: p
-                  }
+                datas    :: a
+                }
 
-instance Functor (Pendulum q) where
-  fmap f (Pendulum m l q p) = Pendulum m l q (f p)
-instance Functor2 Pendulum where
-  fmap1 f (Pendulum m l q p) = Pendulum m l (f q) p
+--newtype Pendulum a = Reader (Double,Double) a
+--mass :: Pendulum Double
+--mass = fst =< ask
+--length = snd =< ask
+
+instance Functor Pendulum where
+  fmap f (Pendulum m l a) = Pendulum m l (f a)
 
 instance HamiltonianModel Pendulum Double Double where
-  hamiltonian (Pendulum m l q p) = p * p / (2.0 * m * l) - m * 9.8 * l * cos q
-  dqdt (Pendulum m l q p) = p / (m * l * l)
-  dpdt (Pendulum m l q p) = - m * 9.8 * l * sin q
-  addQ dq (Pendulum m l q p) = Pendulum m l (q .+^ dq) p
-  addP dp (Pendulum m l q p) = Pendulum m l q (p .+^ dp)
+  hamiltonian (Pendulum m l (q,p)) = p * p / (2.0 * m * l) - m * 9.8 * l * cos q
+  dqdt (Pendulum m l (q,p)) = p / (m * l * l)
+  dpdt (Pendulum m l (q,p)) = - m * 9.8 * l * sin q
 
-class (forall a . Functor (f a)) => Functor2 f where
-  fmap1 :: (a -> a') -> f a b -> f a' b
-  fmap2 :: (b -> b') -> f a b -> f a b'
-  fmap2 = fmap
-
-class (AffineSpace q, AffineSpace p) => HamiltonianModel a q p where
-  hamiltonian :: a q p -> Double
-  dqdt  :: a q p -> Diff q
-  dpdt  :: a q p-> Diff p
-  addQ  :: Diff q -> a q p -> a q p
-  addP  :: Diff p -> a q p -> a q p
-  qEvol :: (v ~ Diff q, VectorSpace v) => Scalar v -> a q p -> a q p
-  qEvol dt apq = addQ (dt *^ dqdt apq) apq
-  pEvol :: (v ~ Diff p, VectorSpace v) => Scalar v -> a q p -> a q p
-  pEvol dt apq = addP (dt *^ dpdt apq) apq
-  symplecticEvol :: (v ~ Diff p, v ~ Diff q, VectorSpace v) => Scalar v -> a q p -> a q p
+class (Functor a, AffineSpace q, AffineSpace p) => HamiltonianModel a q p where
+  hamiltonian :: a (q,p) -> Double
+  dqdt  :: a (q,p) -> Diff q
+  dpdt  :: a (q,p)-> Diff p
+  addQ  :: Diff q -> a (q,p) -> a (q,p)
+  addQ dq = fmap $ first (.+^ dq)
+  addP  :: Diff p -> a (q,p) -> a (q,p)
+  addP dp = fmap $ second (.+^ dp)
+  qEvol :: (v ~ Diff q, VectorSpace v) => Scalar v -> a (q,p) -> a (q,p)
+  qEvol dt aqp = addQ (dt *^ dqdt aqp) aqp
+  pEvol :: (v ~ Diff p, VectorSpace v) => Scalar v -> a (q,p) -> a (q,p)
+  pEvol dt aqp = addP (dt *^ dpdt aqp) aqp
+  symplecticEvol :: (v ~ Diff p, v ~ Diff q, VectorSpace v) => Scalar v -> a (q,p) -> a (q,p)
   symplecticEvol dt = qEvol dt . pEvol dt
 
