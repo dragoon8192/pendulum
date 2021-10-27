3f79b231de227cc4cece716fbe4da9118b8967bb
dragoon8192_mac <dragoon8192@gmail.com>
2021-10-28 08:05:52 +0900

    update:   src/Graphics/Gloss/Data/PointedPictures.hs
    data PointedPictures

--- src/Graphics/Gloss/Data/PointedPictures.hs
+++ src/Graphics/Gloss/Data/PointedPictures.hs
@@ -1,4 +1,16 @@
 module Graphics.Gloss.Data.PointedPictures (
-
+  PointedPictures,
+  toPictures
 ) where
 import Graphics.Gloss
+import Graphics.Gloss.Data.Picture
+
+newtype PointedPictures = PointedPictures {innerList :: [(Picture, Point, Float)]}
+  deriving (Show, Eq)
+toPictures :: PointedPictures -> Picture
+toPictures = Pictures . foldr combine [] . innerList
+  where
+    combine :: (Picture, Point, Float) -> [Picture] -> [Picture]
+    combine (pic, (x, y), theta) ls = (pic :) $ translate x y . rotate theta <$> ls
+
+
