e65c9eebfadee884ad88d0ac47b078d6b3ffa4fb
dragoon8192_mac <dragoon8192@gmail.com>
2021-10-27 13:45:44 +0900

    update:   package.yaml GeneralizedNewtypeDeriving update:   src/PendlumMonad.hs deriving

--- src/PendlumMonad.hs
+++ src/PendlumMonad.hs
@@ -23,41 +23,74 @@ import Control.Monad.State
 --evolP :: (AffineSpace a, VectorSpace (Diff a)) => Scalar (Diff a) -> Diff a -> PhaseSpace a ()
 --evolP dt dpdt = addP $ dpdt ^* dt
 
+
 class (Monad s, AffineSpace (Q s), AffineSpace (P s), VectorSpace (Diff (Q s)), VectorSpace (Diff (P s)), DTime s ~ Scalar (Diff (P s)), DTime s ~ Scalar (Diff (Q s))) => PhysSystem s where
   type DTime s :: *
   type Q s :: *
   type P s :: *
+
+  getPhase :: s (Q s, P s)
   getQ :: s (Q s)
+  getQ = fmap fst getPhase
   getP :: s (P s)
+  getP = fmap snd getPhase
+
+  getDiffPhase :: s (Diff (Q s), Diff (P s))
+  getDqDt :: s (Diff (Q s))
+  getDqDt = fmap fst getDiffPhase
+  getDpDt :: s (Diff (P s))
+  getDpDt = fmap snd getDiffPhase
+
   evolQ :: DTime s -> s ()
   evolP :: DTime s -> s ()
-  getDqDt :: s (Diff (Q s))
-  getDpDt :: s (Diff (P s))
   symplecticEvol1 :: DTime s -> s ()
   symplecticEvol1 dt = do
     evolQ dt
     evolP dt
 
-type Pendulum = ReaderT (Double,Double) (State (Double,Double))
-runPendulum :: Pendulum a -> Double -> Double -> Double -> Double -> (a, (Double, Double))
-runPendulum pendA m l q p = runState (runReaderT pendA (m,l)) (q,p)
-execPendulum :: Pendulum a -> Double -> Double -> Double -> Double -> (Double, Double)
-execPendulum pendA m l q p = execState (runReaderT pendA (m,l)) (q,p)
-getMass :: Pendulum Double
-getMass = asks $ fst
-getLength :: Pendulum Double
-getLength = asks $ snd
+newtype PhysicalSystem d q p x = PhysicalSystem (ReaderT (d, (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p)) (State (q, p)) x)
+  deriving (Functor, Applicative, Monad, MonadState (q,p))
+  --, MonadReader (d, (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p))
+--data PhysicalSystem d q p x where
+--  PhysicalSystem :: (AffineSpace q, AffineSpace p, VectorSpace (Diff q), VectorSpace (Diff p))
+--    => {innerState :: ReaderT (d, (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p)) (State (q, p)) x }
+--    -> PhysicalSystem d q p x
+runPhysicalSystem :: PhysicalSystem d q p x -> d -> (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p) -> (q, p) -> (x, (q, p))
+runPhysicalSystem (PhysicalSystem system) d (dqdt, dpdt) (q, p) = runState (runReaderT system (d, (dqdt, dpdt))) (q, p)
+execPhysicalSystem :: PhysicalSystem d q p x -> d -> (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p) -> (q, p) -> (q, p)
+execPhysicalSystem (PhysicalSystem system) d (dqdt, dpdt) (q, p) = execState (runReaderT system (d, (dqdt, dpdt))) (q, p)
+askData :: (AffineSpace q, AffineSpace p, VectorSpace (Diff q), VectorSpace (Diff p)) => PhysicalSystem d q p d
+askData = PhysicalSystem $ asks fst
+askDiffFunc :: (AffineSpace q, AffineSpace p, VectorSpace (Diff q), VectorSpace (Diff p)) => PhysicalSystem d q p (d -> (q, p) -> Diff q, d -> (q, p) -> Diff p)
+askDiffFunc = PhysicalSystem $ asks snd
 
-instance PhysSystem Pendulum where
-  type DTime Pendulum = Double
-  type Q Pendulum = Double
-  type P Pendulum = Double
-  getQ = lift . gets $ fst
-  getP = lift . gets $ snd
-  getDqDt = dqdt <$> getP <*> getMass <*> getLength
-    where
-      dqdt p m l = p / (m * l * l)
-  getDpDt = dpdt <$> getQ <*> getMass <*> getLength
-    where
-      dpdt q m l = - m * 9.8 * l * sin q
+instance (AffineSpace q, AffineSpace p, VectorSpace (Diff q), VectorSpace (Diff p), Scalar (Diff q) ~ Scalar (Diff p)) => PhysSystem (PhysicalSystem d q p) where
+  type DTime (PhysicalSystem d q p) = Scalar (Diff q)
+  type Q (PhysicalSystem d q p) = q
+  type P (PhysicalSystem d q p) = p
+  getDiffPhase = do
+    (dqdtFunc, dpdtFunc) <- askDiffFunc
+    dqdt <- dqdtFunc <$> askData <*> getPhase
+    dpdt <- dpdtFunc <$> askData <*> getPhase
+    return (dqdt ,dpdt)
+  getPhase = PhysicalSystem $ lift get
+
+
+
+
+
+type Pendulum = PhysicalSystem (Double,Double) Double Double
+
+--instance PhysSystem Pendulum where
+--  --type DTime Pendulum = Double
+--  type Q Pendulum = Double
+--  type P Pendulum = Double
+--  getQ = lift . gets $ fst
+--  getP = lift . gets $ snd
+--  getDqDt = dqdt <$> getP <*> getMass <*> getLength
+--    where
+--      dqdt p m l = p / (m * l * l)
+--  getDpDt = dpdt <$> getQ <*> getMass <*> getLength
+--    where
+--      dpdt q m l = - m * 9.8 * l * sin q
 
